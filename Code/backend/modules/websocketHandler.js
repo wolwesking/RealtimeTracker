fdfe5fbe535d96013b92129d605134c27f3dbc81
wolwesking <83821890+wolwesking@users.noreply.github.com>
2023-11-06 07:48:52 -0800

    fixedDashboard

--- Code/backend/modules/websocketHandler.js
+++ Code/backend/modules/websocketHandler.js
@@ -1,25 +1,16 @@
 const viewCounter = require("../modules/userCounter");
-// IMPORT DATABASE
 
 let msg;
-let cacheMgs = [];
+let cachedData = null; // New variable to store cached data
 
 function init(server, WebSocket) {
   server.on("connection", (ws, request) => {
     const protocols = request.headers["sec-websocket-protocol"];
     if (protocols === "dashboard") {
-      if (cacheMgs.length !== 0) {
-        cacheMgs.forEach((dataFromCache) => {
-          server.clients.forEach((client) => {
-            if (
-              client.readyState === WebSocket.OPEN &&
-              client.protocol === "dashboard"
-            ) {
-              // Convert the JSON object back to a string before sending
-              client.send(JSON.stringify(dataFromCache.newDataToSend));
-            }
-          });
-        });
+      if (cachedData) {
+        // If there's cached data, send it to the new dashboard
+        ws.send(JSON.stringify(cachedData.newDataToSend));
+        cachedData = null; // Clear the cached data
       }
     } else {
       viewCounter.incrementCounter();
@@ -28,7 +19,7 @@ function init(server, WebSocket) {
       // Check if dashboard connected
 
       msg = JSON.parse(message);
-      // Making message to json
+      // Making message to JSON
       const currentCount = viewCounter.getCounter();
 
       const newDataToSend = {
@@ -45,45 +36,41 @@ function init(server, WebSocket) {
         ) {
           // Convert the JSON object back to a string before sending
           client.send(JSON.stringify(newDataToSend));
-          const newCache = {
+          cachedData = {
             ws,
             newDataToSend,
-          };
-          cacheMgs.push(newCache);
+          }; // Cache the data for the initial dashboard
         }
       });
     });
     // Handling closing
 
     ws.on("close", () => {
-      // Loop through the cacheMgs
-      cacheMgs.forEach((data, index) => {
-        if (data.ws === ws) {
-          viewCounter.decrementCounter();
-          const currentCount = viewCounter.getCounter();
-          const leavingDataMsg = {
-            currentCount,
-            type: "leave",
-            msg: data.newDataToSend.msg,
-          };
+      if (protocols === "dashboard") {
+        cachedData = null;
+      } else {
+        viewCounter.decrementCounter();
+        const currentCount = viewCounter.getCounter();
+        const leavingDataMsg = {
+          currentCount,
+          type: "leave",
+          msg: msg, // Include the "leave" message data
+        };
 
-          server.clients.forEach((client) => {
-            if (
-              client.readyState === WebSocket.OPEN &&
-              client.protocol === "dashboard"
-            ) {
-              // Convert the JSON object back to a string before sending
-              client.send(JSON.stringify(leavingDataMsg));
-            }
-          });
-
-          // Remove user from the array
-          cacheMgs.splice(index, 1);
-        }
-      });
+        server.clients.forEach((client) => {
+          if (
+            client.readyState === WebSocket.OPEN &&
+            client.protocol === "dashboard"
+          ) {
+            // Convert the JSON object back to a string before sending
+            client.send(JSON.stringify(leavingDataMsg));
+          }
+        });
+      }
     });
   });
 }
+
 module.exports = {
   init,
 };
